e5e23f619aa189f902efefb83e56f5bd5261807c
samuelfer <samuelfesant@gmail.com>
2022-03-21 23:59:37 -0300

    Add rota para componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -5,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
