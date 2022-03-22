e2ac060c430f6748b001f05018b3c1c02893207f
samuelfer <samuelfesant@gmail.com>
2022-03-21 22:35:24 -0300

    Add rota para componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      {
+        path: 'home', component: HomeComponent
+      }
+    ]
   }
 ];
 
