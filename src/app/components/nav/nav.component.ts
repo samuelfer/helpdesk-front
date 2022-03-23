6c12cf5ba4542991c0878f5b288650fdbbe495c6
samuelfer <samuelfesant@gmail.com>
2022-03-23 20:47:45 -0300

    Ajustes no css

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,13 +17,13 @@ export class NavComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/read/1']);
+    this.router.navigate(['home']);
   }
 
   logout() {
     this.router.navigate(['login']);
     this.authService.logout();
-    this.toast.info('Logout realizado com sucesso', 'Logout', { timeOut: 7000 });
+    this.toast.info('Logout realizado com sucesso', 'Logout');
   }
 
 }
