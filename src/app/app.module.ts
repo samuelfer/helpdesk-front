d49c4cbdb21df5e4a8b973d04de4af1f5efebe90
samuelfer <samuelfesant@gmail.com>
2022-03-23 19:52:16 -0300

    Add componente chamado-update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecn
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 //Componentes
 @NgModule({
@@ -56,6 +57,7 @@ import { ChamadoCreateComponent } from './components/chamado/chamado-create/cham
     ClienteDeleteComponent,
     ChamadoListComponent,
     ChamadoCreateComponent,
+    ChamadoUpdateComponent,
   ],
   imports: [
     BrowserModule,
